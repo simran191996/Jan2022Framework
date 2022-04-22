ef2bdb3b2c6a5554ec2e76ba6422332b2f4b7b12
simran191996 <simrannare@gmail.com>
2022-04-23 00:51:12 +0530

    Updated cartpage as per comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,7 +1,16 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
+	
+	private By cart =By.id("cart");
+	
+	public void addtoCart() {
+		
+	System.out.println("Add to cart is done");
+	}
 	public static void main(String[] args) {
 		int i=10;
 		System.out.println("i");
